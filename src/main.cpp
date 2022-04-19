dfae1333103a9e89c19910ff32d826b85e112a4b
Vinicius-SO <vhisoares39@gmail.com>
2022-04-19 17:39:16 -0300

    Refactor of switch

--- src/main.cpp
+++ src/main.cpp
@@ -15,110 +15,21 @@
 int menu = 0;
 bool ligaCooler = false;
 
-
-void shiftRegisterValue(int value){
-
-  digitalWrite(latchPin, LOW);
-
-  shiftOut(dataPin, clockPin, MSBFIRST, value);
-  
-  digitalWrite(latchPin, HIGH);
-}
-
-void atualizateMenu(){
-  //Switch case responsavel por ligar os leds conforme o menu selecionado
-  switch (menu)
-  {
-    case 1 :
-      shiftRegisterValue(1);
-      Serial.println("Menu 1 acessado");
-      break;
-
-    case 2 :
-      shiftRegisterValue(2);
-      Serial.println("Menu 2 acessado");
-      break;
-    
-    case 3 :
-      shiftRegisterValue(4);
-      Serial.println("Menu 3 acessado");
-      break;
-     
-    case 4 :
-     shiftRegisterValue(8);
-      Serial.println("Menu 4 acessado");
-      break;
-    
-    case 5 :
-      shiftRegisterValue(16);
-      Serial.println("Menu 5 acessado");
-      break;
-    case 6 :
-      shiftRegisterValue(32);
-      Serial.println("Menu 6 acessado");
-      break;
-
-    case 7 :
-      shiftRegisterValue(64);
-      Serial.println("Menu 7 acessado");
-      break;
-    
-    case 8 :
-      digitalWrite(led9, LOW);
-      shiftRegisterValue(128);
-      Serial.println("Menu 8 acessado");
-      break;
-     
-    case 9 :  
-      shiftRegisterValue(0);
-      digitalWrite(led10, LOW);
-      digitalWrite(led9, HIGH);
-      Serial.println("Menu 9 acessado");
-      break;
-    
-    case 10 :
-      digitalWrite(led9, LOW);
-      digitalWrite(led10, HIGH);
-      Serial.println("Menu 10 acessado");
-      break;
-  
-    default:
-      Serial.println("Reset menu, temperature off");
-      PORTD= B00000000;
-      shiftRegisterValue(0);
-
-      break;
-  }
-  delay(200);
-}
-
-
-void incrementMenu(){
-  
-  //Verificação responsável por incrementar o menu e resetar ele apóschegar ao ultimo 
-  if (menu <=9)
-  {
-    menu++;
-  }
-  atualizateMenu();
-  
-}
-
-void decrementMenu(){
-  
-  //Verificação responsável por decrementar o menus
-  if (menu >=1)
-  {
-    menu--;
-  }
-  atualizateMenu();
-  
-}
+byte acendeLed[10];
 
 void setup() {
-
   //Inicia a serial para 9600 
   Serial.begin(9600);
+
+  acendeLed[0] = 0; 
+  acendeLed[1] = 1; 
+  acendeLed[2] = 2; 
+  acendeLed[3] = 4; 
+  acendeLed[4] = 8; 
+  acendeLed[5] = 16; 
+  acendeLed[6] = 32; 
+  acendeLed[7] = 64;
+  acendeLed[8] = 128;
 
   //Define os leds de que representarão o menu, como saída
   DDRD = B11111111;
@@ -127,7 +38,6 @@ void setup() {
   pinMode(selectSubtractButton, INPUT);
   pinMode(selectAddButton, INPUT);
   pinMode(turnOnCooler,INPUT);
-
 }
 
 void loop() {
@@ -150,3 +60,55 @@ void loop() {
   }
 
 }
+
+
+void shiftRegisterValue(int value){
+
+  digitalWrite(latchPin, LOW);
+
+  shiftOut(dataPin, clockPin, MSBFIRST, value);
+  
+  digitalWrite(latchPin, HIGH);
+}
+
+void atualizateMenu(){
+
+  if (menu == 9){
+    digitalWrite(led10, LOW);
+    digitalWrite(led9, HIGH);
+    shiftRegisterValue(acendeLed[0]);
+    Serial.println("Menu 9 acessado");
+  }else if(menu == 10){
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, HIGH);
+    shiftRegisterValue(acendeLed[0]);
+    Serial.println("Menu 10 acessado");
+  }else{
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+    shiftRegisterValue(acendeLed[menu]);
+    Serial.println("Menu acessado:");
+    Serial.println(menu);
+  }
+  
+  delay(200);
+}
+
+void incrementMenu(){
+  //Verificação responsável por incrementar o menu e resetar ele apóschegar ao ultimo 
+  if (menu <=9)
+  {
+    menu++;
+  }
+  atualizateMenu();
+}
+
+void decrementMenu(){
+  //Verificação responsável por decrementar o menus
+  if (menu >=1)
+  {
+    menu--;
+  }
+  atualizateMenu();
+}
+
